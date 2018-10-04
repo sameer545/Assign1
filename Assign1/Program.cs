98934a6a08be643cb7a55c7097beb99c7bb1dcab
sameer545 <abdull.sameerr@gmail.com>
2018-10-04 20:01:51 +0530

    Add project files.

---
+++ Assign1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Assign1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("Enter a number");
+            n = int.Parse(Console.ReadLine());
+            
+            OddEven(n);
+        }
+
+        public static void OddEven(int n)
+        {
+            if(n%2==0)
+            {
+                Console.WriteLine("The number is even ", +n);
+            }
+            else
+            {
+                Console.WriteLine("The number is odd", +n);
+            }
+        }
+    }
+}
